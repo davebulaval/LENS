99dd82d36f30a65ab00d6fb3aaac52a38b23c243
davebulaval <david.beauchemin.5@ulaval.ca>
2023-04-24 14:07:15 -0400

    bump version and revemo stuff in setup

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@ from setuptools import setup
 
 setup(
     name="lens",
-    version="0.1.0",
+    version="0.2.0",
     description="A new metric for text simplification",
-    author="Mounica Maddela",
-    author_email="mmaddela3@gatech.edu",
-    url="https://github.com/Yao-Dou/LENS",
-    download_url="https://github.com/Yao-Dou/LENS/archive/refs/tags/v0.1.0.tar.gz",
-    license="Apache license",
     packages=["lens"],
     install_requires=[
         "sentencepiece",
@@ -21,9 +16,5 @@ setup(
         "torchmetrics >= 0.7",
         "sacrebleu >= 2.0.0",
         "scipy >=1.5.4",
-    ],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Science/Research",
     ],
 )
